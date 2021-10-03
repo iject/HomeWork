0de21f48e35a22804b25b438e80a96ae6c4ac868
iject <91547956+iject@users.noreply.github.com>
2021-10-03 14:44:39 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cin >> a;
+	if ((a > 99 && a < 1000) || (a > -1000 && a < -99)) cout << "Число трехзначное";
+	else cout << "Число не трехзначное";
+}
