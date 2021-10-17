5759e1362276e37c1b6a376b526b4f7e72178d97
iject <91547956+iject@users.noreply.github.com>
2021-10-17 18:31:23 +0300

    Add files via upload

---
+++ HomeWork_2/3(edit).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum(int n)
+{
+    int s = 1;
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            s += i;
+        }
+    }
+    return s;
+}
+
+int main()
+{
+    int s, last = 0;
+    for (int i = 1; i < 10000; i++)
+    {
+        s = sum(i);
+        for (int j = 1; j < 10000; j++)
+        {
+            if (s == j && sum(j) == i && i != last)
+            {
+                last = j;
+                cout << i << " " << j << endl;
+                i += 1;
+            }
+        }
+    }
+}
