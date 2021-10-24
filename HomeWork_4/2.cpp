05e85c2f8ab5d06bb2f12ad479dbf6854c9f49e6
iject <91547956+iject@users.noreply.github.com>
2021-10-24 16:17:42 +0300

    Add files via upload

---
+++ HomeWork_4/2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void print_array(int array[], const int size)
+{
+    cout << "Массив размера " << size << ": " << "\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << "array[" << i << "] = " << array[i] << "\n";
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    const int n = 20;
+    int array[n];
+
+    int i_rand;
+
+    srand(time(0));
+
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = i + 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int tmp;
+        i_rand = rand() % n;
+
+        tmp = array[i];
+        array[i] = array[i_rand];
+        array[i_rand] = tmp;
+    }
+
+    print_array(array, n);
+}
