3f214bfc23da58b42adc77a8d26602518563acf1
iject <91547956+iject@users.noreply.github.com>
2022-10-05 10:13:22 +0300

    Add files via upload

---
+++ class_tochek/class_tochek/Program.cs
@@ -0,0 +1,20 @@
+﻿using class_tochek;
+
+Point a = new Point();
+Point b = new Point(9, 8, 7);
+Point c = new Point(1, 12, -49);
+Point d = new Point(4, 9, -1);
+Point f = new Point(-852, 20, 7);
+
+Console.WriteLine(a.X);
+Console.WriteLine($"b {b}");
+Console.WriteLine($"c {c}");
+Console.WriteLine($"d {d}");
+
+Console.WriteLine("distance(c, d): {0}", Point.distance(c, d));
+Console.WriteLine("distance_r(c): {0}", Point.distance_r(c));
+Console.WriteLine("equation(c, d): {0}", Point.equation(c, d));
+Console.WriteLine("smult(d, f): {0}", Point.smult(d, f));
+Console.WriteLine("vmult(d, f): {0}", Point.vmult(d, f));
+Console.WriteLine("equation_pl(c, d, f): {0}", Point.equation_pl(c, d, f));
+Console.WriteLine(d + f);
