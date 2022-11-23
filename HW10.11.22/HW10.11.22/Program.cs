1f1d2c7da15de74dbb93f6b5657ca6905c390516
iject <91547956+iject@users.noreply.github.com>
2022-11-23 15:11:22 +0300

    Add files via upload

---
+++ HW10.11.22/HW10.11.22/Program.cs
@@ -0,0 +1,11 @@
+﻿using HW10._11._22;
+
+RMatrix m = new RMatrix(6, 6);
+m.show_d();
+
+Console.WriteLine(m[1, 2]);
+m[1, 2] = "0";
+m[1, 0] = "str";
+m.show_d();
+
+m.sosed(0, 0);
---
+++ HW10.11.22/HW10.11.22/RMatrix.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW10._11._22
+{
+    public class RMatrix
+    {
+        // поле
+        private int n;
+        private int m;
+        private Dictionary<int[], string> dict = new Dictionary<int[], string>();
+
+        // индексатор
+        public string this [int i, int j]
+        {
+            get
+            {
+                int[] a = new int[] { i, j };
+
+                if(i < n && j < m)
+                {
+                    foreach (var el in dict)
+                    {
+                        if (a[0] == el.Key[0] && a[1] == el.Key[1])
+                        {
+                            return el.Value;
+                        }
+                    }
+                    return "0";
+                }
+                throw new Exception("Не существует такого индекса");
+            }
+            set
+            {
+                int[] a = new int[] { i, j };
+                foreach (var el in dict)
+                {
+                    if (a[0] == el.Key[0] && a[1] == el.Key[1])
+                    {
+                        dict.Remove(el.Key);
+                        if (value.ToString() != "0") dict.Add(a, value.ToString());
+                        return;
+                    }
+                }
+                if (value.ToString() != "0") dict.Add(a, value.ToString());
+            }
+        }
+
+        public RMatrix() // c клавы
+        {
+
+        }
+
+        public RMatrix(int n, int m) // для рандомного заполнения
+        {
+            Random r = new Random();
+            int k;
+            this.n = n;
+            this.m = m;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    k = (int)r.NextInt64(0, 2);
+                    if(k != 0)
+                    {
+                        dict.Add(new int[] { i, j }, k.ToString());
+                    }
+                }
+            }
+        }
+
+        public void show_d() // вывод dict
+        {
+            foreach (var i in dict)
+            {
+                Console.WriteLine($"key: {i.Key[0]}, {i.Key[1]}  value: {i.Value}");
+            }
+        }
+
+        public void sosed(int j, int i)
+        {
+            Console.WriteLine($"Соседи {i}, {j}");
+            Console.WriteLine($"key: {(i - 1 + n) % n}, {(j - 1 + m) % m}  value: {this[(i - 1 + n) % n, (j - 1 + m) % m]}");
+            Console.WriteLine($"key: {i}, {(j - 1 + m) % m}  value: {this[i, (j - 1 + m) % m]}");
+            Console.WriteLine($"key: {(i + 1 + n) % n}, {(j - 1 + m) % m}  value: {this[(i + 1 + n) % n, (j - 1 + m) % m]}");
+            Console.WriteLine($"key: {(i - 1 + n) % n}, {j}  value: {this[(i - 1 + n) % n, j]}");
+            Console.WriteLine($"key: {i}, {j}  value: {this[i, j]}");
+            Console.WriteLine($"key: {(i + 1 + n) % n}, {j}  value: {this[(i + 1 + n) % n, j]}");
+            Console.WriteLine($"key: {(i - 1 + n) % n}, {(j + 1 + m) % m}  value: {this[(i - 1 + n) % n, (j + 1 + m) % m]}");
+            Console.WriteLine($"key: {i}, {(j + 1 + m) % m}  value: {this[i, (j + 1 + m) % m]}");
+            Console.WriteLine($"key: {(i + 1 + n) % n}, {(j + 1 + m) % m}  value: {this[(i + 1 + n) % n, (j + 1 + m) % m]}");
+        }
+    }
+}
